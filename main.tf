4345dd99c48014250ddc95fe318c6de52bd415f7
Brent Mills <brent.mills@vyopta.com>
2018-06-12 01:10:04 -0500

    adding subnets

--- main.tf
+++ main.tf
@@ -1,28 +1,48 @@
 provider "aws" {
-  region     = "us-east-1"
+  region     = "${var.aws_region}"
 }
 
 data "aws_ami" "centos7" {
   most_recent = true
-
   filter {
     name   = "name"
     values = ["CentOS Linux 7*"]
   }
-
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
   }
-
   owners = ["679593333241"] # Canonical
 }
 
+
+
+resource "aws_subnet" "dcos-prv" {
+  count             = "${length(var.private_subnet_azs)}"
+  vpc_id            = "${var.vpc_id}"
+  cidr_block        = "${lookup(var.private_subnet_azs[count.index], "cidr")}"
+  availability_zone = "${var.aws_region}${lookup(var.private_subnet_azs[count.index], "zone")}"
+  tags {
+    Name = "DCOS Prv ${upper(lookup(var.private_subnet_azs[count.index], "zone"))}"
+  }
+}
+
+resource "aws_subnet" "dcos-pub" {
+  count             = "${length(var.public_subnet_azs)}"
+  vpc_id            = "${var.vpc_id}"
+  cidr_block        = "${lookup(var.public_subnet_azs[count.index], "cidr")}"
+  availability_zone = "${var.aws_region}${lookup(var.public_subnet_azs[count.index], "zone")}"
+  tags {
+    Name = "DCOS Pub ${upper(lookup(var.public_subnet_azs[count.index], "zone"))}"
+  }
+}
+
 resource "aws_instance" "dcos-slave" {
+    count                         = 1
     ami                           = "${data.aws_ami.centos7.id}"
     instance_type                 = "m5.large"
     vpc_security_group_ids        = ["sg-58523722"]
-    subnet_id                     = "subnet-37f7327f"
+    subnet_id                     = "${element(aws_subnet.dcos-prv.*.id, count.index)}"
     associate_public_ip_address   = "false"
     key_name                      = "DevOps"
     iam_instance_profile          = "DCOS"
@@ -34,7 +54,6 @@ resource "aws_instance" "dcos-slave" {
         volume_type = "gp2"
         volume_size = "50"
     }
-    
     provisioner "remote-exec" {
         inline = ["echo connected"]
 
@@ -44,7 +63,6 @@ resource "aws_instance" "dcos-slave" {
             private_key = "${file(var.ssh_key_private_file)}"
         }
     }
-
     provisioner "local-exec" {
         command = "ansible-playbook -u ${var.ssh_user} -i '${self.private_ip},' --private-key ${var.ssh_key_private_file} provision.yml" 
     }
--- variables.tf
+++ variables.tf
@@ -1,9 +1,37 @@
+variable "aws_region" {
+  description = "The region the cluster will be created in"
+  default = "us-east-1"
+}
+
+variable "vpc_id" {
+  description = "Id of the VPC used to create the cluster in"
+  default = "vpc-6b86db0c"
+}
+
+variable "private_subnet_azs" {
+  description = "List of subnet configs for availability zones"
+  default = [
+    {"zone" = "c", "cidr" = "172.29.11.0/26"},
+    {"zone" = "d", "cidr" = "172.29.11.64/26"},
+    {"zone" = "e", "cidr" = "172.29.11.128/26"}
+  ]
+}
+
+variable "public_subnet_azs" {
+  description = "List of subnet configs for availability zones"
+  default = [
+    {"zone" = "c", "cidr" = "172.29.10.0/26"},
+    {"zone" = "d", "cidr" = "172.29.10.64/26"},
+    {"zone" = "e", "cidr" = "172.29.10.128/26"}
+  ]
+}
+
 variable "ssh_user" {
-	description	= "The user for connecting to an instance"
-	default		= "devops"
+  description = "The user for connecting to an instance"
+  default = "devops"
 }
 
 variable "ssh_key_private_file" {
-	description = "The location for the private key for the ssh user"
-	default = "~/.ssh/qa.key"
+  description = "The location for the private key for the ssh user"
+  default = "~/.ssh/qa.key"
 }
